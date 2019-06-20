14836121f4f67ac7d62fd2ac63c976d550806776
SAFI <amine.safi@esprit.tn>
2019-06-20 19:50:02 +0100

    ratings data service micro-service first commit

---
+++ src/main/java/tn/aminesafi/ratingsdataservice/models/UserRating.java
@@ -0,0 +1,23 @@
+package tn.aminesafi.ratingsdataservice.models;
+
+import java.util.List;
+
+public class UserRating {
+
+	private List<Rating> userRating;
+	
+	
+	public UserRating() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public List<Rating> getUserRating() {
+		return userRating;
+	}
+
+	public void setUserRating(List<Rating> userRating) {
+		this.userRating = userRating;
+	}
+}
